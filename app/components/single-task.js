3d2439eb5c1573f79d541bf4dd789fb104402a0b
Raymond Yegon <raymondyegon17@gmail.com>
2020-02-17 08:39:31 +0300

    consumed data from routes file dynamically

--- app/components/single-task.js
+++ app/components/single-task.js
@@ -2,4 +2,10 @@ import Component from '@glimmer/component';
 
 export default
   class SingleTaskComponent extends Component {
+  isPinned = false
+
+  @action
+  moveUp() {
+    this.toggleProperty('isPinned');
+  }
 }
